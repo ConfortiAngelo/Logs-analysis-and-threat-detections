0ae38df59f46604df1df8368fb997d3bf66fcedd
AngeloConforti <confortiangelod@gmail.com>
2025-03-06 13:02:37 -0300

    Creacion de logs y avances en codigo log_reader.py

--- logs_analysis/log_reader.py
+++ logs_analysis/log_reader.py
@@ -1,13 +1,15 @@
-import os , re , glob
-
-
+import os
 
 def read_logs(path):
     if os.path.exists(path):
         with open(path,'r') as file:
-            return file.read()
+            for line in file:
+                content = line.strip()
+                return content
     else:
         return "Error , el folder no existe!!"
 
-# print(read_logs("C:/Users/angel/Desktop/CURSO"))
-print(os.path.exists("C:/Users/angel/Desktop/CURSO"))
+path = "/home/angelo/Escritorio/CURSO/Cibersecurity/Logs analysis and threat detection/data/sample_log_1.log"
+# for line in read_logs(path):
+#     print(line)
+
---
+++ logs_analysis/threat_detection.py
@@ -0,0 +1,4 @@
+import re, log_reader
+
+path = "/home/angelo/Escritorio/CURSO/Cibersecurity/Logs analysis and threat detection/data/sample_log_1.log"
+print(log_reader.read_logs(path))
